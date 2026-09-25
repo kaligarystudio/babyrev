e17621174a7d0091e1d6ca1c83a032c9298a61ea
kaligarystudio <kaligarystudio@gmail.com>
2026-09-25 10:11:53 -0600

    Add HomePage component with title and description

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+export default function HomePage() {
+  return (
+    <main>
+      <h1>Baby Runner</h1>
+
+      <p>
+        La aventura del bebé está por comenzar.
+      </p>
+    </main>
+  );
+}
